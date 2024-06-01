d0ea622839f87a848811ffd01939eb8cdf26fbe4
Corlius <corliuswang@gmail.com>
2024-06-01 18:54:33 +0800

    revise

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
+//--------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------
+//----------------------    ██████╗ ██████╗ ██████╗ ██╗     ██╗██╗   ██╗███████╗   -----------------------
+//----------------------   ██╔════╝██╔═══██╗██╔══██╗██║     ██║██║   ██║██╔════╝   -----------------------
+//----------------------   ██║     ██║   ██║██████╔╝██║     ██║██║   ██║███████╗   -----------------------
+//----------------------   ██║     ██║   ██║██╔══██╗██║     ██║██║   ██║╚════██║   -----------------------
+//----------------------   ╚██████╗╚██████╔╝██║  ██║███████╗██║╚██████╔╝███████║   -----------------------
+//----------------------    ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝ ╚═════╝ ╚══════╝   -----------------------
+//----------------------                                                           -----------------------
+//----------------------            ██╗    ██╗ █████╗ ███╗   ██╗ ██████╗           -----------------------
+//----------------------            ██║    ██║██╔══██╗████╗  ██║██╔════╝           -----------------------
+//----------------------            ██║ █╗ ██║███████║██╔██╗ ██║██║  ███╗          -----------------------
+//----------------------            ██║███╗██║██╔══██║██║╚██╗██║██║   ██║          -----------------------
+//----------------------            ╚███╔███╔╝██║  ██║██║ ╚████║╚██████╔╝          -----------------------
+//----------------------             ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝           -----------------------
+//--------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------
+//-- If you need to customize the tray items, search engines and wallpapers, edit the following arrays. --
+//--------------------------------------------------------------------------------------------------------
+
+// Tray items array
+const trayIconsItems = [
+    { src: 'trayicons/ChatGPT.png', alt: 'ChatGPT', url: 'https://chatgpt.com/' },
+    { src: 'trayicons/AIStudio.png', alt: 'AIStudio', url: 'https://aistudio.google.com/app/prompts/1pJD5v--kDEq-gPjlqngb2ByzIcvpX7__' },
+    { src: 'trayicons/GitHub.png', alt: 'GitHub', url: 'https://github.com/' },
+    { src: 'trayicons/YouTube.png', alt: 'YouTube', url: 'https://www.youtube.com/' },
+    { src: 'trayicons/Bilibili.png', alt: 'Bilibili', url: 'https://www.bilibili.com/' },
+    { src: 'trayicons/Disney+.png', alt: 'Disney+', url: 'https://www.disneyplus.com/' },
+    { src: 'trayicons/Appstorrent.png', alt: 'Appstorrent', url: 'https://appstorrent.ru/programs/' }
+];
+
+// Search engings array
+const searchEngines = [
+    {
+        name: 'Google',
+        url: 'https://www.google.com/search',
+        queryParam: 'q'
+    },
+    {
+        name: 'Bing',
+        url: 'https://www.bing.com/search',
+        queryParam: 'q'
+    },
+    {
+        name: 'Baidu',
+        url: 'https://www.baidu.com/s',
+        queryParam: 'wd'
+    }
+];
+
+// Wallpapers array
 const wallpapers = [
     "wallpapers/DarkAbstractReflection.jpg",
     "wallpapers/DarkGlassCircleWithReflection.jpg",
@@ -47,29 +98,15 @@ const wallpapers = [
     "wallpapers/LightWaves.jpg",
     "wallpapers/LightWavesBlue.jpg",
     "wallpapers/LightWavesCyan.jpg",
-    ];
-
-const searchEngines = [
-    {
-        name: 'Google',
-        url: 'https://www.google.com/search',
-        icon: 'google.png',
-        queryParam: 'q'
-    },
-    {
-        name: 'Bing',
-        url: 'https://www.bing.com/search',
-        icon: 'bing.png',
-        queryParam: 'q'
-    },
-    {
-        name: 'Baidu',
-        url: 'https://www.baidu.com/s',
-        icon: 'baidu.png',
-        queryParam: 'wd'
-    }
 ];
 
+
+
+//---------------------------------------------------------------------
+//-- Generally, the following contents does not need to be modified. --
+//---------------------------------------------------------------------
+
+// Time update function
 function updateTime() {
     const timeDisplay = document.querySelector('.time-display');
     const now = new Date();
@@ -79,21 +116,61 @@ function updateTime() {
     timeDisplay.textContent = timeString;
 }
 
+// Default values
 const defaultLightWallpaper = 'wallpapers/LightGlass.jpg';
 const defaultDarkWallpaper = 'wallpapers/DrakCirclePurple.jpg';
 const defaultSearchEngine = 'Google';
 
+// Selector values
+const selectedSearchEngine = localStorage.getItem('searchEngine') || defaultSearchEngine;
+const lightWallpaper = localStorage.getItem('lightWallpaper') || defaultLightWallpaper;
+const darkWallpaper = localStorage.getItem('darkWallpaper') || defaultDarkWallpaper;
+
+// Update current wallpaper
+document.documentElement.style.setProperty('--light-wallpaper', `url(${lightWallpaper})`);
+document.documentElement.style.setProperty('--dark-wallpaper', `url(${darkWallpaper})`);
+
+// Run after DOM loaded
 document.addEventListener('DOMContentLoaded', () => {
-    const selectedSearchEngine = localStorage.getItem('searchEngine') || defaultSearchEngine;
-    const lightWallpaper = localStorage.getItem('lightWallpaper') || defaultLightWallpaper;
-    const darkWallpaper = localStorage.getItem('darkWallpaper') || defaultDarkWallpaper;
-
-    document.documentElement.style.setProperty('--light-wallpaper', `url(${lightWallpaper})`);
-    document.documentElement.style.setProperty('--dark-wallpaper', `url(${darkWallpaper})`);
-
+    // Update clock every 100ms
     updateTime();
     setInterval(updateTime, 100);
 
+    // Search box part
+    const searchBox = document.querySelector('.search-box');
+    const searchInput = document.querySelector('.search-box input[type="text"]');
+
+    // Use '/' to activate search box
+    document.onkeydown = function(e) {
+        const focusElement = document.activeElement;
+        if (e.key === '/' && focusElement !== searchInput) {
+            e.preventDefault();
+            searchInput.focus();
+        }
+    };
+
+    // Scale up on active
+    searchInput.addEventListener('focus', () => {
+        searchBox.classList.add('active');
+    });
+
+    // Scale down on inactive
+    searchInput.addEventListener('blur', () => {
+        searchBox.classList.remove('active');
+    });
+
+    // Listen esc
+    document.addEventListener('keydown', (event) => {
+        if (event.key === 'Escape' && searchBox.classList.contains('active')) {
+            searchBox.classList.remove('active');
+            searchInput.blur();
+        }
+    });
+
+    // Focus on search box while loaded
+    searchInput.focus();
+
+    // Define search button dunction
     const searchButton = document.querySelector('.search-button');
     searchButton.addEventListener('auxclick', (event) => {
         if (event.button === 1) {
@@ -107,51 +184,11 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    const searchInput = document.querySelector('.search-box input[type="text"]');
-    const searchBox = document.querySelector('.search-box');
-    searchInput.focus();
-    document.onkeydown = function(e) {
-        const focusElement = document.activeElement;
-        if (e.key === '/' && focusElement !== searchInput) {
-            e.preventDefault();
-            searchInput.focus();
-        }
-    };
+    // Tray part
+    const trayIcons = document.querySelector('.tray-icons');
 
-    searchInput.addEventListener('focus', () => {
-        searchBox.classList.add('active');
-    });
-
-    searchInput.addEventListener('blur', () => {
-        searchBox.classList.remove('active');
-    });
-
-    searchBox.classList.add('active');
-
-    document.addEventListener('keydown', (event) => {
-        if (event.key === 'Escape' && searchBox.classList.contains('active')) {
-            searchBox.classList.remove('active');
-            searchInput.blur();
-        }
-    });
-
-    const tray = document.createElement('div');
-    tray.className = 'tray';
-
-    const trayIcons = document.createElement('div');
-    trayIcons.className = 'tray-icons';
-
-    const trayIconsItem = [
-        { src: 'trayicons/ChatGPT.png', alt: 'ChatGPT', url: 'https://chatgpt.com/' },
-        { src: 'trayicons/AIStudio.png', alt: 'AIStudio', url: 'https://aistudio.google.com/app/prompts/1pJD5v--kDEq-gPjlqngb2ByzIcvpX7__' },
-        { src: 'trayicons/GitHub.png', alt: 'GitHub', url: 'https://github.com/' },
-        { src: 'trayicons/YouTube.png', alt: 'YouTube', url: 'https://www.youtube.com/' },
-        { src: 'trayicons/Bilibili.png', alt: 'Bilibili', url: 'https://www.bilibili.com/' },
-        { src: 'trayicons/Disney+.png', alt: 'Disney+', url: 'https://www.disneyplus.com/' },
-        { src: 'trayicons/Appstorrent.png', alt: 'Appstorrent', url: 'https://appstorrent.ru/programs/' }
-    ];
-
-    trayIconsItem.forEach(config => {
+    // Add items to tray
+    trayIconsItems.forEach(config => {
         const icon = document.createElement('img');
         icon.className = 'tray-icon';
         icon.src = config.src;
@@ -166,28 +203,46 @@ document.addEventListener('DOMContentLoaded', () => {
         trayIcons.appendChild(icon);
     });
 
-    tray.appendChild(trayIcons);
-    document.body.appendChild(tray);
-
+    // Settings part
     const settingsButton = document.querySelector('.settings-icon img');
     const settingsMenu = document.querySelector('.settings-menu');
-    const lightwallpaperselect = document.querySelector('#light-wallpaper-select');
-    const darkwallpaperselect = document.querySelector('#dark-wallpaper-select');
     const searchEngineSelect = document.querySelector('#search-engine-select');
     const searchForm = document.querySelector('.search-box form');
+    const lightwallpaperselect = document.querySelector('#light-wallpaper-select');
+    const darkwallpaperselect = document.querySelector('#dark-wallpaper-select');
 
+    // Listen settings icon click
     settingsButton.addEventListener('click', (event) => {
         const isVisible = settingsMenu.style.display === 'block';
         settingsMenu.style.display = isVisible ? 'none' : 'block';
         event.stopPropagation();
     });
 
+    // Click any other place to close
     document.addEventListener('click', (event) => {
         if (settingsMenu.style.display === 'block' && !settingsMenu.contains(event.target)) {
             settingsMenu.style.display = 'none';
         }
     });
 
+    // Generate search engins selection items
+    searchEngines.forEach(engine => {
+        const option = document.createElement('option');
+        option.value = engine.name;
+
+        const wrapper = document.createElement('div');
+        wrapper.className = 'search-engine-option';
+
+        const span = document.createElement('span');
+        span.textContent = engine.name;
+
+        wrapper.appendChild(span);
+        option.appendChild(wrapper);
+
+        searchEngineSelect.appendChild(option);
+    });
+
+    // Generate wallpapers selection items
     wallpapers.forEach(file => {
         const option = document.createElement('option');
         option.value = file;
@@ -205,51 +260,35 @@ document.addEventListener('DOMContentLoaded', () => {
         darkwallpaperselect.appendChild(option.cloneNode(true));
     });
 
-    searchEngines.forEach(engine => {
-        const option = document.createElement('option');
-        option.value = engine.name;
-
-        const wrapper = document.createElement('div');
-        wrapper.className = 'search-engine-option';
-
-        const img = document.createElement('img');
-        img.src = engine.icon;
-        img.alt = engine.name;
-
-        const span = document.createElement('span');
-        span.textContent = engine.name;
-
-        wrapper.appendChild(img);
-        wrapper.appendChild(span);
-        option.appendChild(wrapper);
-
-        searchEngineSelect.appendChild(option);
-    });
-
+    // Apply current selections to the selectors
     searchEngineSelect.value = selectedSearchEngine;
     lightwallpaperselect.value = lightWallpaper;
     darkwallpaperselect.value = darkWallpaper;
 
     updateSearchFormAction(selectedSearchEngine);
 
+    // Listen search engine sclector
     searchEngineSelect.addEventListener('change', (e) => {
         const selectedEngine = e.target.value;
         updateSearchFormAction(selectedEngine);
         localStorage.setItem('searchEngine', selectedEngine);
     });
 
+    // Listen light wallpaper selector
     lightwallpaperselect.addEventListener('change', (e) => {
         const selectedWallpaper = e.target.value;
         document.documentElement.style.setProperty('--light-wallpaper', `url(${selectedWallpaper})`);
         localStorage.setItem('lightWallpaper', selectedWallpaper);
     });
 
+    // Listen dark wallpaper slector
     darkwallpaperselect.addEventListener('change', (e) => {
         const selectedWallpaper = e.target.value;
         document.documentElement.style.setProperty('--dark-wallpaper', `url(${selectedWallpaper})`);
         localStorage.setItem('darkWallpaper', selectedWallpaper);
     });
 
+    // Update search form action
     function updateSearchFormAction(selectedEngine) {
         const selectedEngineData = searchEngines.find(engine => engine.name === selectedEngine);
         searchForm.action = selectedEngineData.url;
@@ -261,31 +300,34 @@ document.addEventListener('DOMContentLoaded', () => {
         };
     }
 
+    // Preview function
     const previewButton = document.getElementById('previewButton');
-    const closeModal = document.getElementById('closeModal');
     const previewModal = document.getElementById('wallpaperPreviewModal');
+    const closeModal = document.getElementById('closeModal');
     const galleryContainer = document.getElementById('gallery');
     let largeImage = null;
-    let loadingIndex = 0;
-    let loadedImages = [];
+    previewModal.style.transition = 'opacity 0.2s ease-in-out';
 
+    // Get pieview pic path
     function getPreviewImagePath(imagePath) {
         const fileName = imagePath.split('/').pop();
         return `wallpapers/preview/${fileName}`;
     }
 
+    // Click to open modal
     previewButton.addEventListener('click', (event) => {
         event.stopPropagation();
         openPreviewModal(); 
     });
+    // Click to close modal
     closeModal.addEventListener('click', closePreviewModal);
 
-    previewModal.style.transition = 'opacity 0.2s ease-in-out';
-
+    // Click any where else to cloes modal
     function closeModalOnOutsideClick(event) {
         if (!previewModal.contains(event.target)) closePreviewModal();
     }
 
+    // Function to open modal
     function openPreviewModal() {
         previewModal.style.opacity = '0';
         previewModal.style.display = 'block';
@@ -303,8 +345,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 img.src = getPreviewImagePath(fullPath);
                 img.alt = fileName;
                 img.dataset.fullImage = fullPath;
-
                 div.appendChild(img);
+
                 const span = document.createElement('span');
                 span.textContent = fileName.replace(/^[^-]*-/, '');
                 div.appendChild(span);
@@ -312,32 +354,52 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         }
         window.addEventListener('click', closeModalOnOutsideClick);
+        document.addEventListener('keydown', (event) => {
+            if (event.key === 'Escape') {
+                closePreviewModal();
+            }
+        });
     }
 
+    // Function to close modal
     function closePreviewModal() {
         previewModal.style.opacity = '0';
         setTimeout(() => {
             previewModal.style.display = 'none';
+            if (largeImage) {
+                largeImage.remove();
+                largeImage = null;
+            }
         }, 200);
-        if (largeImage) {
-            largeImage.classList.remove('large');
-            largeImage = null;
-        }
         window.removeEventListener('click', closeModalOnOutsideClick);
+        document.removeEventListener('keydown', (event) => {
+            if (event.key === 'Escape') {
+                closePreviewModal();
+            }
+        });
     }
-
+    
+    
+    // Click to view original
     galleryContainer.addEventListener('click', (event) => {
         if (event.target.tagName === 'IMG') {
             const img = event.target;
-            if (largeImage) {
-                largeImage.classList.remove('large');
-                largeImage.src = getPreviewImagePath(largeImage.dataset.fullImage);
-                largeImage = null;
-            } else {
-                img.classList.add('large');
-                img.src = img.dataset.fullImage;
-                largeImage = img;
-            }
+
+            const largeImageContainer = document.createElement('div');
+            largeImageContainer.classList.add('large-image-container');
+            previewModal.appendChild(largeImageContainer);
+
+            largeImage = document.createElement('img');
+            largeImage.src = img.dataset.fullImage;
+            largeImage.classList.add('large');
+            largeImageContainer.appendChild(largeImage);
+
+            largeImage.addEventListener('click', (event) => {
+                event.stopPropagation();
+                largeImage.remove();
+                largeImage = null; 
+                previewModal.removeChild(largeImageContainer);
+            });
         }
     });
 });
