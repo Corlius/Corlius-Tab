67f3be9ef174ce70669853fe2ca2440b34939b9a
Corlius <corliuswang@gmail.com>
2024-11-07 13:58:30 +0800

    add HeyTap

--- script.js
+++ script.js
@@ -25,7 +25,8 @@ const trayIconsItems = [
     { src: 'trayicons/GitHub.png', alt: 'GitHub', url: 'https://github.com/' },
     { src: 'trayicons/YouTube.png', alt: 'YouTube', url: 'https://www.youtube.com/' },
     { src: 'trayicons/Bilibili.png', alt: 'Bilibili', url: 'https://www.bilibili.com/' },
-    { src: 'trayicons/Disney+.png', alt: 'Disney+', url: 'https://www.disneyplus.com/' },
+    // { src: 'trayicons/Disney+.png', alt: 'Disney+', url: 'https://www.disneyplus.com/' },
+    { src: 'trayicons/HeyTap.png', alt: 'HeyTap', url: 'https://cloud.heytap.com/' },
     { src: 'trayicons/MacKed.png', alt: 'MacKed', url: 'https://macked.app/' },
     { src: 'trayicons/Appstorrent.png', alt: 'Appstorrent', url: 'https://appstorrent.ru/programs/' }
 ];
